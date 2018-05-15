0a834150b9e423ec22a9765853d8f15e330d98e3
Michelle Steigerwalt <msteigerwalt@gmail.com>
2018-05-15 17:24:14 -0400

    Snakecase.

--- riddle.py
+++ riddle.py
@@ -15,19 +15,21 @@ Returns the crossword in ASCII form for a provided word by hitting the URL.
 
 Returns False if the URL is not valid.
 """
-def getCrossword(word, url="recurseriddle.com"):
+def get_crossword(word, url="recurseriddle.com"):
     pass
 
 """
 Takes an ASCII crossword and returns a two dimensional array of its
 tile content.
 """
-def getGrid(crossword):
-    pass
+def get_grid(crossword):
+
 
 """
 Takes a two dimensional array of tile content and returns its result
 when rotated clockwise.
 """
-def rotateGrid(grid):
+def rotate_grid(grid):
     pass
+
+
